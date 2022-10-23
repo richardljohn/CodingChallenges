ba0333731e61b1e936dbce55f83035f0654547cd
richardljohn <richardljohn@gmail.com>
2022-10-23 13:47:19 -0400

    Leetcode #344

---
+++ reverseString.py
@@ -0,0 +1,12 @@
+# Leetcode Challenge #344 - Reverse String 
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        stack = []
+        for i in s:
+            stack.append(i)
+        for i in range(len(s)):
+            s[i] = stack.pop()
