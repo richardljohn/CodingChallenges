4da213d237d6eb91d73f28d908826bde154e56b4
richardljohn <richardljohn@gmail.com>
2022-10-23 12:46:46 -0400

    Leetcode Challenge #108

---
+++ sortedArrayToBST.py
@@ -0,0 +1,24 @@
+# Leetcode Challenge #108 - Sorted Array to BST
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        
+        def makeTree(left, right):
+            if left > right: 
+                return None
+            
+            mid = (left + right) // 2 
+            root = TreeNode(nums[mid])
+            root.left = makeTree(left, mid - 1)
+            root.right = makeTree(mid + 1, right)
+
+            return root
+        
+        return makeTree(0, len(nums) - 1)
