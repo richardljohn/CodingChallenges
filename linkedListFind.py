7a42b153678b9790f487afd8a2b6b62a2511e3cf
richardljohn <richardljohn@gmail.com>
2022-11-09 21:44:04 -0500

    More Linked List Stuff

---
+++ linkedListFind.py
@@ -0,0 +1,23 @@
+class Node: 
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+
+a.next = b
+b.next = c
+c.next = d
+
+def linkedListFind(head, target):
+    if head is not None:
+        curr = head 
+        while curr is not None: 
+            if curr.val == target:
+                return True 
+            curr = curr.next
+        return False 
+    return False
