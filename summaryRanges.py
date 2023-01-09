438389906f14f645c6efee8ec7a2cfe6a7824cd5
richardljohn <richardljohn@gmail.com>
2023-01-08 20:52:56 -0500

    More Problems.?

---
+++ summaryRanges.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        start, curr, end = nums[0], nums[0], None
+        res = []
+        for n in nums[1:]:
+            curr += 1
+            if n == curr: 
+                end = n 
+            else:
+                if not end: 
+                    res.append(str(start))
+                else: 
+                    res.append(str(start) + "->" + str(end))
+                start = n 
+                end = n 
+                end = None
+            if not end: 
+                res.append(str(start))
+            else: 
+                res.append(str(start)+"->"+str(end))
+            return res
+
+
