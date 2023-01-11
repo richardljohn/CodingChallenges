2848ad3d5bbfcf3a7449115c1cddab61f5a94933
richardljohn <richardljohn@gmail.com>
2023-01-11 15:31:50 -0500

    Another Challenge Done.

--- twoSumLessThanK.py
+++ twoSumLessThanK.py
@@ -9,4 +9,8 @@ def twoSumLess(nums, k):
                 maxNum = sum
     return maxNum if maxNum >= 0 else -1
 
+# Test Case 1: Will output 58
 print(twoSumLess([34, 23, 1, 24, 75, 33, 54, 8], 60))
+
+# Test Case 2: Will output -1
+print(twoSumLess([10, 20, 30], 15))
