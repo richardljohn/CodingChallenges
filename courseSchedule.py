ceef69338da54712919ad9ac77bcdd1f3e65cefd
richardljohn <richardljohn@gmail.com>
2023-03-18 00:06:34 -0400

    LeetCode #207

--- courseSchedule.py
+++ courseSchedule.py
@@ -7,3 +7,23 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
+
+        preMap = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        
+        visitSet = set()
+
+        def dfs(crs):
+            if crs in visitSet: 
+                return False
+            if preMap[crs] == []: 
+                return True 
+            
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            
+            visitSet.remove(crs)
+            return True
