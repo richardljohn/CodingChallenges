89c1499187c421741f87d92f16a4c6a2dacb010f
richardljohn <richardljohn@gmail.com>
2023-03-17 23:54:41 -0400

    LeetCode #207

--- courseSchedule.py
+++ courseSchedule.py
@@ -1,1 +1,9 @@
 # LeetCode #207 - Course Schedule
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
