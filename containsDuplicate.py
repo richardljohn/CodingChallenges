a7f313f33ed1304f35aed7ee66c0de086c57409e
richardljohn <richardljohn@gmail.com>
2023-03-19 18:40:31 -0400

    LeetCode #217

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,1 +1,9 @@
 # LeetCode #217 - Contains Duplicate
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
