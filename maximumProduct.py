eb54e2010aa8ee2cab66f3c1296e7ff2ba3d9d3c
richardljohn <richardljohn@gmail.com>
2023-03-20 22:21:32 -0400

    LeetCode #153 - Maximum Product

---
+++ maximumProduct.py
@@ -0,0 +1,12 @@
+# LeetCode #152 - Maximum Product
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+
+        
