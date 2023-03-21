ea289df152111b0a16e5995398d1e1a02b01c9b4
richardljohn <richardljohn@gmail.com>
2023-03-20 22:18:58 -0400

    LeetCode #53 Test Cases Passed

--- maximumSubarray.py
+++ maximumSubarray.py
@@ -1,5 +1,22 @@
 # LeetCode #53 - Maximum Subarray
 
+class Solution(object):
+    def maxSubArray(self, nums):
+            """
+            :type nums: List[int]
+            :rtype: int
+            """
+
+            maxSub = nums[0]
+            currSum = 0
+
+            for num in nums:
+                if currSum < 0:
+                    currSum = 0
+                currSum += num
+                maxSub = max(maxSub, currSum)
+            return maxSub
+
 def maxSubArray(nums):
         """
         :type nums: List[int]
