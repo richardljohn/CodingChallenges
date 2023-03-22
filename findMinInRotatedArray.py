9e60e4c5b19fbaa8a3e91cd97c7ad1b027d3d9aa
richardljohn <richardljohn@gmail.com>
2023-03-21 20:41:19 -0400

    LeetCode #153 - Find Min. in Rotated Sorted Array (Test Cases)

--- findMinInRotatedArray.py
+++ findMinInRotatedArray.py
@@ -9,7 +9,7 @@ def findMin(nums):
         res = nums[0]
         l, r = 0, len(nums) - 1 
         while l <= r:
-            if nums[l] > nums[r]:
+            if nums[l] < nums[r]:
                 res = min(res, nums[l])
                 break 
 
