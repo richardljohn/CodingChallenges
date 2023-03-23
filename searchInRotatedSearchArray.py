edb5472644a9bdaf6068131e290f2a73f3df4574
richardljohn <richardljohn@gmail.com>
2023-03-22 23:22:39 -0400

    LeetCode #33 Done

--- searchInRotatedSearchArray.py
+++ searchInRotatedSearchArray.py
@@ -26,8 +26,3 @@ class Solution(object):
                 else: 
                     l = mid + 1
         return -1
-
-
-searcher = Solution()
-# Test Cases 
-print(searcher.search([4, 5, 6, 7, 0, 1, 2], 0))
