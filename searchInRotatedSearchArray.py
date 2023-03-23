d705a9c31a007ad29bca2801c35524bee3d12082
richardljohn <richardljohn@gmail.com>
2023-03-22 23:11:46 -0400

    LeetCode #33

--- searchInRotatedSearchArray.py
+++ searchInRotatedSearchArray.py
@@ -7,6 +7,23 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        mid = len(nums)/ 2
+        l, r = 0, len(nums) - 1
 
-        if nums[mid] > 
+        while l <= r: 
+
+            mid = (l + r) / 2
+            if target == nums[mid]:
+                return mid
+            
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else: 
+                    r = mid - 1
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1 
+                else: 
+                    l = mid + 1
+        
+        return -1
