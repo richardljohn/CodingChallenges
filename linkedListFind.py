64167fbff21e721e844e41e0292a346ce966f9de
richardljohn <richardljohn@gmail.com>
2023-03-23 23:11:14 -0400

    LeetCode #141 (Test Cases)

--- linkedListFind.py
+++ linkedListFind.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, val):
         self.val = val
         self.next = None
+        
 
 a = Node("a")
 b = Node("b")
--- listCycle.py
+++ listCycle.py
@@ -1,5 +1,11 @@
 # LeetCode #141 - Linked List Cycle 
 
+# Definition for singly-linked list.
+class Node(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
 #class Solution(object):
 def hasCycle(head):
     """
@@ -16,3 +22,12 @@ def hasCycle(head):
     return False
 
 #Test Cases 
+a = Node(3)
+b = Node(2)
+c = Node(0)
+d = Node(4)
+a.next = b
+b.next = c
+c.next = d
+d.next = b
+print(hasCycle(a))
