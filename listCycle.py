8b18c4b5916b4a43d53933ab0407b4b81acabdce
richardljohn <richardljohn@gmail.com>
2023-03-23 23:06:38 -0400

    LeetCode #141

--- listCycle.py
+++ listCycle.py
@@ -1,8 +1,16 @@
 # LeetCode #141 - Linked List Cycle 
 
-class Solution(object):
-    def hasCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
+#class Solution(object):
+def hasCycle(head):
+    """
+    :type head: ListNode
+    :rtype: bool
+    """
+    visited = set()
+
+    while head != None:
+        if head in visited:
+            return True
+        visited.add(head)
+        head = head.next
+    return False
