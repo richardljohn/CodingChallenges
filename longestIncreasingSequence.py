c15e1b68dc813196781db15d069f3a388b0ea352
richardljohn <richardljohn@gmail.com>
2023-04-10 23:29:19 -0400

    LeetCode 300 TestCases Done.

--- longestIncreasingSequence.py
+++ longestIncreasingSequence.py
@@ -16,7 +16,7 @@ class Solution(object):
 
 s = Solution()
 
-# Test Cases
+# Test Cases (passed)
 print(s.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])) # 4
 print(s.lengthOfLIS([0, 1, 0, 3, 2, 3])) # 4 
 print(s.lengthOfLIS([7, 7, 7, 7, 7, 7, 7])) # 1
