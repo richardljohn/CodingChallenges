e02b0dad14383d1a6763e5b8abb4e989bc966934
richardljohn <richardljohn@gmail.com>
2023-04-10 23:28:37 -0400

    LeetCode 300 TestCases

--- longestIncreasingSequence.py
+++ longestIncreasingSequence.py
@@ -7,9 +7,16 @@ class Solution(object):
         :rtype: int
         """
         LIS = [1] * len(nums)
-        
+
         for i in range(len(nums) - 1, -1, -1):
             for j in range(i + 1, len(nums)):
                 if nums[i] < nums[j]:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
         return max(LIS)
+
+s = Solution()
+
+# Test Cases
+print(s.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
+print(s.lengthOfLIS([0, 1, 0, 3, 2, 3]))
+print(s.lengthOfLIS([7, 7, 7, 7, 7, 7, 7]))
