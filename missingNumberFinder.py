742e1d38388334d50d61f22e16be4cb290f3c8a2
richardljohn <richardljohn@gmail.com>
2023-04-30 16:51:50 -0400

    Leetcode #268 - Missing Number

---
+++ missingNumberFinder.py
@@ -0,0 +1,14 @@
+# Leetcode #268 - Missing Number
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += (i - nums[i])
+        return res
