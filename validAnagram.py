fe6f3883ed28021842682c500229ad76ff6bf684
richardljohn <richardljohn@gmail.com>
2023-05-01 14:09:46 -0400

    Leetcode #242 - Valid Anagram

---
+++ validAnagram.py
@@ -0,0 +1,24 @@
+# LeetCode #242 - Valid Anagram
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t): 
+            return False
+
+        i = 0
+        sMap, tMap = {}, {}
+        while i < len(s):
+            sMap[s[i]] = 1 + sMap.get(s[i], 0)
+            tMap[t[i]] = 1 + tMap.get(t[i], 0)
+            i += 1 
+        
+        for c in sMap:
+            if sMap[c] != tMap.get(c, 0):
+                return False
+        
+        return True
