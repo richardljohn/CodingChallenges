6c2dd38dd110bd739d6494b2f0b857027aa778e0
richardljohn <richardljohn@gmail.com>
2023-05-04 19:04:19 -0400

    LeetCode 21

--- mergeTwoLists.py
+++ mergeTwoLists.py
@@ -17,4 +17,21 @@ class Solution(object):
 
         dummyHead = ListNode(0)
         head = dummyHead
+        headOne, headTwo = list1, list2
 
+        while headOne is not None and headTwo is not None:
+            if headOne.val > headTwo.val: 
+                dummyHead.next = ListNode(headOne.val)
+                headOne = headOne.next
+                dummyHead = dummyHead.next
+            else:
+                dummyHead.next = ListNode(headTwo.val)
+                headTwo = headTwo.next
+                dummyHead = dummyHead.next
+        
+        if headOne is None:
+            dummyHead.next = headTwo
+        if headTwo is None: 
+            dummyHead.next = headOne
+        
+        return head.next
