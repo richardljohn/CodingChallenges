52a5aaf99a6f4b92d0cb7d89db4b69903b2631e3
richardljohn <richardljohn@gmail.com>
2023-05-04 19:01:22 -0400

    LeetCode 21

---
+++ mergeTwoLists.py
@@ -0,0 +1,20 @@
+# Leetcode #21 - Merge Two Sorted Lists
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        dummyHead = ListNode(0)
+        head = dummyHead
+
