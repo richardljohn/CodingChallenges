6b26ec3bc81e171f30058551d08d736165df2c3b
richardljohn <richardljohn@gmail.com>
2023-05-04 19:21:05 -0400

    LeetCode 125 - Valid Palindrome

--- validPalindrome.py
+++ validPalindrome.py
@@ -7,12 +7,22 @@ class Solution(object):
         :rtype: bool
         """
     
-        s = ""
+        word = ""
         i = 0
         while i < len(s):
             if s[i].isalpha():
-                s += s[i].lower()
+                word += s[i].lower()
                 i += 1
             else: 
                 i += 1
         
+        if word == word[::-1]:
+            return True
+        else:
+            return False
+
+
+
+s = Solution()
+
+print(s.isPalindrome("0P"))
