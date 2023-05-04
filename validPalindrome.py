12f109d777a3409376384789ecc233de4f807b0f
richardljohn <richardljohn@gmail.com>
2023-05-04 19:13:15 -0400

    LeetCode 125 - Valid Palindrome

--- validPalindrome.py
+++ validPalindrome.py
@@ -6,3 +6,13 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+    
+        s = ""
+        i = 0
+        while i < len(s):
+            if s[i].isalpha():
+                s += s[i].lower()
+                i += 1
+            else: 
+                i += 1
+        
