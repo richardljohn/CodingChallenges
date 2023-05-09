02e741991cebb3cbb225d60d08e9a21cabebe158
richardljohn <richardljohn@gmail.com>
2023-05-08 22:33:15 -0400

    Leetcode 1

--- twoSum.py
+++ twoSum.py
@@ -1,4 +1,5 @@
 # Leetcode 1 - Two Sum
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -6,3 +7,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        l = 0
+        r = 1
+        count = 0 
+
+        while l < len(nums):
+            while r < len(nums):
+                if nums[l] + nums[r] == target:
+                    return True
+                r += 1 
+            l += 1
