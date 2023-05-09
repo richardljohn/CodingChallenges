caa6327d037fdd906841cb97439c154275e8afe9
richardljohn <richardljohn@gmail.com>
2023-05-08 22:30:21 -0400

    Leetcode 1

--- twoSum.py
+++ twoSum.py
@@ -1,13 +1,1 @@
-#Leetcode Challenge #1 - Two Sum
-
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        numMap = {} 
-
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in numMap: 
-                return [numMap[diff], i]
-            numMap[n] = i
-        return
+# Leetcode 1 - Two Sum
