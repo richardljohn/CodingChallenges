b84200f63d7b72b2934949f785e9afb3686374a2
richardljohn <richardljohn@gmail.com>
2023-05-08 22:37:14 -0400

    Leetcode 1

--- twoSum.py
+++ twoSum.py
@@ -15,4 +15,11 @@ class Solution(object):
                 return [prevMap[diff], i]
             prevMap[n] = i
         return 
-        
+
+
+s = Solution()
+
+# Testcases
+print(s.twoSum([2,7,11,15], 9))
+print(s.twoSum([3,2,4], 6))
+print(s.twoSum([3,3], 6))
