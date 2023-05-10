ddd55822291ff5a033e88494a5e412581c830f2d
richardljohn <richardljohn@gmail.com>
2023-05-09 21:00:31 -0400

    Leetcode #23

--- mergeKLists.py
+++ mergeKLists.py
@@ -1,10 +1,13 @@
 # Leetcode 23 - Merge K Sorted Lists
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+        
 class Solution(object):
     def mergeKLists(self, lists):
         """
