3396b64622d739f104bf966ecb5f6e663a2ac352
richardljohn <richardljohn@gmail.com>
2023-05-26 19:59:21 -0400

    Leetcode #14

---
+++ longestCommonPrefix.py
@@ -0,0 +1,8 @@
+# LeetCode #14 - Longest Common Prefix
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
