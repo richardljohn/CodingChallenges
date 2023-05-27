fe3a71a078fd2b8e726dff4fc97cd2e7d3fd6d42
richardljohn <richardljohn@gmail.com>
2023-05-26 20:07:31 -0400

    Leetcode #14

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -6,3 +6,13 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+
+        res = ""
+        
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+
+        return res
