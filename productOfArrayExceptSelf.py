a2c4357f834164cd17a6a3605da2d23cd0b98822
richardljohn <richardljohn@gmail.com>
2023-10-17 12:53:23 -0400

    LeetCode #238

--- productOfArrayExceptSelf.py
+++ productOfArrayExceptSelf.py
@@ -7,5 +7,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+
+        result = [1] * len(nums)
+
+        prefix = 1 
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix = prefix * nums[i]
+
+        postfix = 1 
+        for i in range(len(nums) -1, -1, -1):
+            result[i] = result[i] * postfix
+            postfix = postfix * nums[i]
+        
+        return result
         
 s = Solution()
