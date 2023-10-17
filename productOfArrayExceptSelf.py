351bf9ee12030d42be1609de4b3b4a41d9ec7102
richardljohn <richardljohn@gmail.com>
2023-10-17 12:34:14 -0400

    LeetCode #238

--- productOfArrayExceptSelf.py
+++ productOfArrayExceptSelf.py
@@ -1,2 +1,11 @@
 # LeetCode #238 - Product of Array Except Self
 
+class Solution(object):
+
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+s = Solution()
