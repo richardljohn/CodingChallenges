bea628d09ddce4066a96682579dadcb6c8df9bc5
richardljohn <richardljohn@gmail.com>
2023-10-26 18:25:23 -0400

    LC167 Done

--- twoSumII.py
+++ twoSumII.py
@@ -22,18 +22,21 @@ class Solution(object):
         
         return []
 
+s = Solution()
 # Example 1:
-
 # Input: numbers = [2,7,11,15], target = 9
 # Output: [1,2]
 # Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
-# Example 2:
+print(s.twoSum([2, 7, 11, 15], 9))
 
+# Example 2:
 # Input: numbers = [2,3,4], target = 6
 # Output: [1,3]
 # Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
-# Example 3:
+print(s.twoSum([2, 3, 4], 6))
 
+# Example 3:
 # Input: numbers = [-1,0], target = -1
 # Output: [1,2]
 # Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+print(s.twoSum([-1, 0], -1))
