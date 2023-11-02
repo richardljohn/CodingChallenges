b2400bf16272bd62f2407de077b12cd7e202a9d6
richardljohn <richardljohn@gmail.com>
2023-11-02 17:04:33 -0400

    Leetcode #15 (Done)

--- threeSum.py
+++ threeSum.py
@@ -58,5 +58,5 @@ print(s.threeSum([0, 0, 0]))
 # nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 # nums.sort()
 
-# for i, a in enumerate(nums): 
+ 
 #     print("i = " + str(i) + ", a = " + str(a))
