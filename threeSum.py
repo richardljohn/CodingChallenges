c8c1b9433f7d1a7d63bbb8cc78e8e7733338c670
richardljohn <richardljohn@gmail.com>
2023-11-02 16:59:34 -0400

    Leetcode #15

--- threeSum.py
+++ threeSum.py
@@ -1,8 +1,38 @@
 # Leetcode #15 - 3 Sum
 
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        res = []
+        nums.sort()
 
-nums = [1, 2, 3, 4, 5]
-nums.sort()
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
 
-for i, a in enumerate(nums): 
-    print("i = " + str(i) + ", a = " + str(a))
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0: 
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else: 
+                    res.append([a, nums[l], nums[r]])
+                    l += 1 
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return res
+
+
+# nums = [1, 2, 3, 4, 5]
+# nums.sort()
+
+# for i, a in enumerate(nums): 
+#     print("i = " + str(i) + ", a = " + str(a))
