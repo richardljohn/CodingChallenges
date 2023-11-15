7df66c6abc75bd50169ac7dd5464ba45af2307be
richardljohn <richardljohn@gmail.com>
2023-11-14 23:30:19 -0500

    LeetCode #128 - Longest Consecutive Sequence

---
+++ longestConsecutiveSequence.py
@@ -0,0 +1,10 @@
+# LeetCode # 128 - Longest Consecutive Sequence
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        numSet = set(nums)
