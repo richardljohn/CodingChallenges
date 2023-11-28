9e3479658a4204756c815e089715b023801b43b2
richardljohn <richardljohn@gmail.com>
2023-11-27 22:53:08 -0500

    Leetcode #11

--- containerWithMostWater.py
+++ containerWithMostWater.py
@@ -1,1 +1,9 @@
 # Leetcode # 11 - Container With Most Water
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        maxWater = 0
