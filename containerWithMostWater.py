785af153bd87c1f7b4121e0bfcf147f42496756c
richardljohn <richardljohn@gmail.com>
2023-11-27 23:04:24 -0500

    Leetcode #11 Brute Force

--- containerWithMostWater.py
+++ containerWithMostWater.py
@@ -8,4 +8,16 @@ class Solution(object):
         """
         maxWater = 0
 
+        # Brute Force
+
+        for l in range(len(height)):
+            for r in range(1 + l, len(height)):
+                area = (r - l) * min(height[l], height[r])
+                maxWater = max(maxWater, area)
+
         return maxWater
+
+S = Solution()
+
+print(S.maxArea([1,8,6,2,5,4,8,3,7]))
+print(S.maxArea([1,1]))
