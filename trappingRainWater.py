855aa05931319ce96d7bfbb1e146204e3ce803d3
richardljohn <richardljohn@gmail.com>
2023-11-28 22:55:19 -0500

    Leetcode #11

--- trappingRainWater.py
+++ trappingRainWater.py
@@ -7,4 +7,13 @@ class Solution:
         l, r = 0, len(height) - 1
         maxLeft, maxRight = 0, 0 
 
-        
+        while l < r: 
+            maxLeft = max(maxLeft, height[l])
+            maxRight = max(maxRight, height[r])
+
+            if maxLeft < maxRight: 
+                l += 1
+                total = maxLeft - height[l]
+                water += total if total > 0 else 0
+            else:
+                r -= 1
