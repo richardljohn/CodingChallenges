0caaad2580ca9976fe72e4a796c5c3addf4ee4ef
richardljohn <richardljohn@gmail.com>
2023-11-28 22:52:02 -0500

    Leetcode #11

--- trappingRainWater.py
+++ trappingRainWater.py
@@ -1,2 +1,10 @@
 # Leetcode #42 - Trapping Rain Water
 
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        water = 0
+        l, r = 0, len(height) - 1
+        maxLeft, maxRight = 0, 0 
+
+        
