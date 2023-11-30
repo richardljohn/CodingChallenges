7a446e78e2856317ff384d14eca5edcbae4b5e39
richardljohn <richardljohn@gmail.com>
2023-11-29 22:09:23 -0500

    Leetcode #121

--- bestTimeToBuyAndSellStock.py
+++ bestTimeToBuyAndSellStock.py
@@ -1,1 +1,10 @@
 # Leetcode #121 - Best Time to Buy and Sell Stock
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        l, r = 0, 1
