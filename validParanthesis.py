8be53bb4d3125500d5c1c3afd1ae5064131a78ff
richardljohn <richardljohn@gmail.com>
2024-01-04 20:38:27 -0500

    Done

--- validParanthesis.py
+++ validParanthesis.py
@@ -1,2 +1,25 @@
 # Leetcode #20 - Valid Paranthesis
 
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+        l, end = 0, len(s)
+        
+        stack = []
+        brackets = { ")" : "(", "]" : "[", "}": "{" }
+
+        for c in s: 
+            if c in brackets: 
+                if stack and stack[-1] == brackets[c]:
+                    stack.pop()
+                else: 
+                    return False
+            else:
+                stack.append(c)
+        
+        return True if not stack else False
