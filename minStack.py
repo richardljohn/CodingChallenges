eb99aec8e3ef05489ad29b4d5374cb32fb16c037
richardljohn <richardljohn@gmail.com>
2024-01-05 22:53:26 -0500

    Done

--- minStack.py
+++ minStack.py
@@ -3,31 +3,39 @@
 class MinStack(object):
 
     def __init__(self):
+        self.mainStack = []
+        self.minStack = []
         
-
     def push(self, val):
         """
         :type val: int
         :rtype: None
         """
+        self.mainStack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
         
 
     def pop(self):
         """
         :rtype: None
         """
+        self.mainStack.pop()
+        self.minStack.pop()
         
 
     def top(self):
         """
         :rtype: int
         """
+        return self.mainStack[-1]
         
 
     def getMin(self):
         """
         :rtype: int
         """
+        return self.minStack[-1]
         
 
 
