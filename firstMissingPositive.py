c814fe27b8482932f4c7cc87ada0e8fc054a407c
richardljohn <richardljohn@gmail.com>
2024-05-31 15:34:22 -0400

    Leetcode 41

--- firstMissingPositive.py
+++ firstMissingPositive.py
@@ -3,6 +3,7 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         
+
         for i in range(len(nums)):
             if nums[i] < 0: 
                 nums[i] = 0
