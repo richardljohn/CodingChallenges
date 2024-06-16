ac03cfea52615249c4fd719e27f72438e3bb2d2b
richardljohn <richardljohn@gmail.com>
2024-06-15 21:03:53 -0400

    Leetcode 271

--- longestRepeatingCharacterReplacement.py
+++ longestRepeatingCharacterReplacement.py
@@ -3,58 +3,6 @@
 # Needed Library
 import collections
 
-
-
-
-# class Solution(object):
-#     def characterReplacement(self, s, k):
-#         """
-#         :type s: str
-#         :type k: int
-#         :rtype: int
-#         """
-
-# Time Complexity :  O(n)
-# Space Complexity : O(1)
-class Solution(object):
-    def characterReplacement(self, s, k):
-        maxlen, largestCount = 0, 0
-        arr = collections.Counter()
-        for idx in range(len(s)):
-            arr[s[idx]] += 1
-            largestCount = max(largestCount, arr[s[idx]])
-            if maxlen - largestCount >= k:
-                arr[s[idx - maxlen]] -= 1
-            else:
-                maxlen += 1
-        return maxlen
-        # count = {}
-        # res = 0
-
-        # l = 0 
-        # maxf = 0
-        # for r in range(len(s)):
-        #     count[s[r]] = 1 + count.get(s[r], 0)
-        #     maxf = max(maxf, count[s[r]])
-        #     while (r - 1 + 1) - maxf > k: 
-        #         count[s[l]] -= 1
-        #         l += 1
-        #     res = max(res, r - l + 1)
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
         
-        # return res
-
-S = Solution()
-
-# Example 1:
-# Input: s = "ABAB", k = 2
-# Output: 4
-# Explanation: Replace the two 'A's with two 'B's or vice versa.
-print(S.characterReplacement("ABAB", 2))
-
-# Example 2:
-# Input: s = "AABABBA", k = 1
-# Output: 4
-# Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
-# The substring "BBBB" has the longest repeating letters, which is 4.
-# There may exists other ways to achieve this answer too.
-print(S.characterReplacement("AABABBA", 1))
