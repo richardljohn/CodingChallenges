4510c571ad08e00df7eefe14bdefa3b44d673b9d
richardljohn <richardljohn@gmail.com>
2024-11-23 16:48:24 -0500

    Leetcode 28

--- findFirstInString.py
+++ findFirstInString.py
@@ -2,4 +2,11 @@
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return 0
+
+        if len(haystack) < len(needle): 
+            return -1
+        
+        for i in range(len(haystack)):
+            if haystack[i:i+len(needle)] == needle:
+                 return i
+        return -1
