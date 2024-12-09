6c824afa998b42b6535b42e1d1e4dc5eb92e6fb0
richardljohn <richardljohn@gmail.com>
2024-12-09 14:16:16 -0500

    Leetcode 215 Done

--- findKthLargest.py
+++ findKthLargest.py
@@ -6,6 +6,7 @@ class Solution:
         return nums[len(nums) - k]
 
 S = Solution()
-
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-print(S.findKthLargest(numbers, 2))
+
+for i in range(len(numbers)):
+    print(S.findKthLargest(numbers, (len(numbers) - i)))
