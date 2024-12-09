c1ddf127b1ffa23dc1da704e7d3a05c1b1ae66cf
richardljohn <richardljohn@gmail.com>
2024-12-09 14:33:27 -0500

    Leetcode 1046 done

--- lastStoneWeight.py
+++ lastStoneWeight.py
@@ -9,7 +9,6 @@ class Solution:
         while len(stones) > 1:
             first = abs(heapq.heappop(stones))
             second = abs(heapq.heappop(stones))
-
             if first > second:
                 heapq.heappush(stones, -1*(first-second))
         
