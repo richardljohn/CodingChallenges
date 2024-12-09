d2a88a4586317552807119ea22e0fe006c0adc46
richardljohn <richardljohn@gmail.com>
2024-12-09 14:33:07 -0500

    Leetcode 1046

---
+++ lastStoneWeight.py
@@ -0,0 +1,18 @@
+# Leetcode #1046 - Last Stone Weight
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = abs(heapq.heappop(stones))
+            second = abs(heapq.heappop(stones))
+
+            if first > second:
+                heap.heappush(stones, -1*(first-second))
+        
+        if not stones:
+            return 0 
+        return abs(stones[0])
