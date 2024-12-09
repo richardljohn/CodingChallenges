3ab6eac22f764fb37243ccb8a40f955e75b6b989
richardljohn <richardljohn@gmail.com>
2024-12-09 14:13:53 -0500

    Leetcode 215 Done

--- findKthLargest.py
+++ findKthLargest.py
@@ -1,6 +1,11 @@
 # Leetcode #215 - Find Kth Largest
 
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        nums.sort()
+        return nums[len(nums) - k]
 
+S = Solution()
 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(S.findKthLargest(numbers, 2))
